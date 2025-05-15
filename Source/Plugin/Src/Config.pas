rdipardo/nppFSIPlugin

commit c2c282d2379034d6575b9ea038f64d0393c16036

Prefer the application's tab preferences when set
Avoids interfering with XML-saved custom tab settings, a feature
extended to external lexers in v8.8.1 (*)

 ---
 (*) notepad-plus-plus/notepad-plus-plus@0ffd606

Source/Plugin/Src/Config.pas
@@ -51,6 +51,9 @@ uses
   Graphics,
   Utf8IniFiles;
 
+const
+  DEFAULT_TAB_SETTING = -1;
+
 type
 
   TPropertyInt = Boolean;
@@ -70,6 +73,8 @@ type
     _tabLength: Integer;
     _echoNPPTextInEditor: TPropertyInt;
     _initialFoldState: TPropertyInt;
+    procedure SetTabPreference(TabPref: TPropertyInt);
+    procedure SetTabLength(TabLength: Integer);
   public
     constructor Create;
     destructor Destroy; override;
@@ -84,8 +89,8 @@ type
     property EditorTextColorDark: TColor read _clStdOutDark write _clStdOutDark;
     property EditorErrorColor: TColor read _clStdErr write _clStdErr;
     property EditorErrorColorDark: TColor read _clStdErrDark write _clStdErrDark;
-    property ConvertTabsToSpacesInFSIEditor: TPropertyInt read _convertTabsToSpacesInFSIEditor write _convertTabsToSpacesInFSIEditor;
-    property TabLength: Integer read _tabLength write _tabLength;
+    property ConvertTabsToSpacesInFSIEditor: TPropertyInt read _convertTabsToSpacesInFSIEditor write SetTabPreference;
+    property TabLength: Integer read _tabLength write SetTabLength;
     property EchoNPPTextInEditor: TPropertyInt read _echoNPPTextInEditor write _echoNPPTextInEditor;
   end;
 
@@ -115,6 +120,7 @@ type
     class property XMLConfig: WideString read GetXMLConfig;
   end;
 
+function TryParseTabSettings(var Value: LongInt): Boolean;
 procedure CopyFile(const SrcPath, DestPath: WideString);
 
 implementation
@@ -124,6 +130,9 @@ uses
   Classes, SysUtils, Windows, Dom, XmlRead,
   // plugin units
   Constants, ModulePath, NppPlugin, FSIPlugin;
+
+var
+  TabPrefValue: LongInt = DEFAULT_TAB_SETTING;
 
 { TConfiguration }
 
@@ -154,8 +163,8 @@ begin
       _passArgsToDotnetFsi := configINI.ReadBool(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_PASS_ARGS_TO_DOTNET_FSI, False);
       _fsiPath := configINI.ReadString(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_BINARY_KEY_NAME, EmptyStr);
       _fsiArgs := configINI.ReadString(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_BINARYARGS_KEY_NAME, EmptyStr);
-      _convertTabsToSpacesInFSIEditor := configINI.ReadBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, True);
-      _tabLength := configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, DEFAULT_TAB_LENGTH);
+      SetTabPreference(configINI.ReadBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, True));
+      SetTabLength(configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, DEFAULT_TAB_LENGTH));
       _echoNPPTextInEditor := configINI.ReadBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_ECHO_NPP_TEXT_KEY_NAME, True);
       _clStdOut := TColor(configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, 'TEXT_COLOR', clBlack));
       _clStdErr := TColor(configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, 'ERROR_TEXT_COLOR', clRed));
@@ -195,6 +204,22 @@ begin
       PWchar('File Reload Required'), MB_ICONINFORMATION);
 end;
 {$ENDREGION}
+
+{%Region 'Private Methods'}
+procedure TConfiguration.SetTabPreference(TabPref: TPropertyInt);
+begin
+  _convertTabsToSpacesInFSIEditor := TabPref;
+  if TryParseTabSettings(TabPrefValue) then
+    _convertTabsToSpacesInFSIEditor := TPropertyInt(TabPrefValue shr 7);
+end;
+
+procedure TConfiguration.SetTabLength(TabLength: Integer);
+begin
+  _tabLength := TabLength;
+  if TryParseTabSettings(TabPrefValue) then
+    _tabLength := TabPrefValue and $7f;
+end;
+{%EndRegion}
 
 { TLexerProperties }
 
@@ -253,8 +278,14 @@ begin
     FoldMultiLineComments := GetProperty(configINI, 'FOLD_MULTILINE_COMMENTS');
     FoldOpenStatements := GetProperty(configINI, 'FOLD_OPEN_STATEMENTS');
     FoldPreprocessor := GetProperty(configINI, 'FOLD_PREPROCESSOR');
-    IndentWithSpaces := configINI.ReadBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, True);
-    IndentWidth := configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, DEFAULT_TAB_LENGTH);
+    if TryParseTabSettings(TabPrefValue) then
+    begin
+      IndentWithSpaces := TPropertyInt(TabPrefValue shr 7);
+      IndentWidth := TabPrefValue and $7f;
+    end else begin
+      IndentWithSpaces := configINI.ReadBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, True);
+      IndentWidth := configINI.ReadInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, DEFAULT_TAB_LENGTH);
+    end;
   finally
     FreeAndNil(configINI);
   end;
@@ -388,6 +419,61 @@ begin
     begin
       MessageBoxW(0, PWideChar(UTF8Decode(E.Message)), PWideChar('Error'), MB_ICONERROR);
     end;
+  end;
+end;
+
+{ ------------------------------------------------------------------------------------------------ }
+function TryParseTabSettings(var Value: LongInt): Boolean;
+var
+  Doc: TXMLDocument;
+  Root, TabSettingsNode: TDOMNode;
+  hXMLFile: THandle;
+  fStream: TStream;
+  ParseResult: Word;
+begin
+  Doc := nil;
+  fStream := nil;
+  Result := False;
+  try
+    hXMLFile := FileOpen(TLexerProperties.XMLConfig, fmOpenRead);
+    if hXMLFile <> THandle(-1) then
+    begin
+      fStream := THandleStream.Create(hXMLFile);
+      try
+        ReadXMLFile(Doc, fStream);
+      except
+        on E: Exception do
+        begin
+        end;
+      end;
+      if Assigned(Doc) then
+      begin
+        Root := Doc.DocumentElement.FirstChild;
+        while Assigned(Root) do
+        begin
+          if UnicodeSameText(Root.NodeName, 'language') then
+          begin
+            if Assigned(Root.Attributes) then
+            begin
+              TabSettingsNode := Root.Attributes.GetNamedItem('tabSettings');
+              if Assigned(TabSettingsNode) then
+              begin
+                Val (TabSettingsNode.NodeValue, Value, ParseResult);
+                Result := (ParseResult = 0) and (Value > 0);
+                Break;
+              end;
+            end;
+          end;
+          Root := Root.FirstChild;
+        end;
+      end;
+    end;
+  finally
+    FileClose(hXMLFile);
+    if Assigned(Doc) then
+      FreeAndNil(Doc);
+    if Assigned(fStream) then
+      FreeAndNil(fStream);
   end;
 end;
 

Source/Plugin/Forms/ConfigForm.pas
@@ -137,6 +137,8 @@ uses
 {$R *.dfm}
 {$ENDIF}
 
+var
+  TabSettingValue: LongInt = Config.DEFAULT_TAB_SETTING;
 
 { TFrmConfiguration }
 
@@ -224,6 +226,10 @@ begin
 end;
 
 procedure TFrmConfiguration.initialize;
+const
+  TabSettingsToolTip = 'Managed by Notepad++'#13#10+
+    'Go to Settings > Preferences > Indentation and select'#13#10+
+    '"Use default value" to enable these options';
 begin
   if not Assigned(_config) then
     _config := TConfiguration.Create;
@@ -231,6 +237,19 @@ begin
   chkPassArgsToDotnetFsi.Checked := _config.UseArgs;
   txtFSIBinary.Text := _config.FSIPath;
   txtFSIBinaryArgs.Text := _config.FSIArgs;
+  if Config.TryParseTabSettings(TabSettingValue) then
+  begin
+    pnlTabSettings.Enabled := False;
+    pnlTabSettings.ShowHint := True;
+    pnlTabSettings.Hint := TabSettingsToolTip;
+  end else begin
+    pnlTabSettings.Enabled := True;
+    pnlTabSettings.ShowHint := False;
+    _config.LoadFromConfigFile;
+  end;
+  chkConvertToTabs.Enabled := pnlTabSettings.Enabled;
+  chkConvertToTabs.ShowHint := pnlTabSettings.ShowHint;
+  chkConvertToTabs.Hint := pnlTabSettings.Hint;
   chkConvertToTabs.Checked := _config.ConvertTabsToSpacesInFSIEditor;
   txtTabLength.Text := IntToStr(_config.TabLength);
   chkEchoText.Checked := _config.EchoNPPTextInEditor;
@@ -264,11 +283,14 @@ begin
   _config.UseArgs := chkPassArgsToDotnetFsi.Checked;
   _config.FSIPath := txtFSIBinary.Text;
   _config.FSIArgs := txtFSIBinaryArgs.Text;
-  _config.ConvertTabsToSpacesInFSIEditor := chkConvertToTabs.Checked;
-  if (chkConvertToTabs.Checked) then
-    _config.TabLength := StrToInt(txtTabLength.Text)
-  else
-    _config.TabLength := DEFAULT_TAB_LENGTH;
+  if chkConvertToTabs.Enabled then
+  begin
+    _config.ConvertTabsToSpacesInFSIEditor := chkConvertToTabs.Checked;
+    if (chkConvertToTabs.Checked) then
+      _config.TabLength := StrToInt(txtTabLength.Text)
+    else
+      _config.TabLength := DEFAULT_TAB_LENGTH;
+  end;
   _config.EchoNPPTextInEditor := chkEchoText.Checked;
   with TLexerProperties do
   begin

commit e5e15c42c6e0c4548a6f2cb501633cc4c66031ba

Keep application tab settings out of the INI file
Fixes a regression caused by c2c282d

Source/Plugin/Src/Config.pas
@@ -187,8 +187,11 @@ begin
     configINI.WriteBool(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_PASS_ARGS_TO_DOTNET_FSI, _passArgsToDotnetFsi);
     configINI.WriteString(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_BINARY_KEY_NAME, _fsiPath);
     configINI.WriteString(CONFIG_FSI_SECTION_NAME, CONFIG_FSI_SECTION_BINARYARGS_KEY_NAME, _fsiArgs);
-    configINI.WriteBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, _convertTabsToSpacesInFSIEditor);
-    configINI.WriteInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, _tabLength);
+    if not TryParseTabSettings(TabPrefValue) then
+    begin
+      configINI.WriteBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABTOSPACES_KEY_NAME, _convertTabsToSpacesInFSIEditor);
+      configINI.WriteInteger(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_SECTION_TABLENGTH_KEY_NAME, _tabLength);
+    end;
     configINI.WriteBool(CONFIG_FSIEDITOR_SECTION_NAME, CONFIG_FSIEDITOR_ECHO_NPP_TEXT_KEY_NAME, _echoNPPTextInEditor);
     configINI.WriteInteger(CONFIG_FSIEDITOR_SECTION_NAME, 'TEXT_COLOR', ColorToRGB(_clStdOut));
     configINI.WriteInteger(CONFIG_FSIEDITOR_SECTION_NAME, 'ERROR_TEXT_COLOR', ColorToRGB(_clStdErr));